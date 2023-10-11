f88435d1c076258c89aea3d9aa2c56a95e936133
Thiago Seixas <thiago.seixas10@gmail.com>
2023-10-11 17:50:15 -0400

    feat: creat routes

--- app/main.py
+++ app/main.py
@@ -1,15 +1,38 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, status
+from typing import List
+from . import actions, models, schemas
+from pydantic import BaseModel
+from app.database import DATABASE_URL, engine
+from fastapi_sqlalchemy import DBSessionMiddleware
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app = FastAPI(title="Credit Card")
+
+app.add_middleware(DBSessionMiddleware, db_url=DATABASE_URL, custom_engine=engine)
+
+class HTTPError(BaseModel):
+    detail: str
+
+    class Config:
+        schema_extra = {
+            "example": {"detail": "HTTPException raised."},
+        }
+
+
+class Message(BaseModel):
+    message: str
+
+
+@app.post("/cards/", response_model=schemas.Card)
+def create_card(card: schemas.Card):
+    return actions.save_card(card=card)
+
+
+@app.get("/cards/", response_model=List[schemas.Card])
+def read_cards(skip: int = 0, limit: int = 10):
+    return actions.get_cards(skip=skip, limit=limit)
+
+
+@app.get("/cards/{card_id}", response_model=schemas.Card, responses={status.HTTP_404_NOT_FOUND: {"model": Message}})
+def read_card(card_id: int):
+    return actions.get_card_by_id(card_id=card_id)
