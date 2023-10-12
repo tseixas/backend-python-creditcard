edb5a3feb2983db222a6f77371d676e8a8df71a1
Thiago Seixas <thiago.seixas10@gmail.com>
2023-10-12 15:33:11 -0400

    feat: improvement url endpoints

--- app/main.py
+++ app/main.py
@@ -12,16 +12,16 @@ app.add_middleware(DBSessionMiddleware, db_url=DATABASE_URL,
                    custom_engine=engine)
 
 
-@app.post("/cards/", response_model=schemas.Card)
+@app.post('/api/v1/credit-card/', response_model=schemas.Card)
 def save_card(card: schemas.CardBase):
     return actions.save_card(card=card)
 
 
-@app.get("/cards/", response_model=schemas.CardList)
+@app.get('/api/v1/credit-card/', response_model=schemas.CardList)
 def list_cards(skip: int = 0, limit: int = 10, page: int = 1):
     return actions.get_cards(skip=skip, limit=limit, page=page)
 
 
-@app.get("/cards/{card_id}", response_model=schemas.Card)
+@app.get('/api/v1/credit-card/{card_id}', response_model=schemas.Card)
 def get_card(card_id: int):
     return actions.get_card_by_id(card_id=card_id)
